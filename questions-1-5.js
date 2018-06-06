578cf3576d20cc9ef69bb0b8b9a870058dfb0790
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-06 17:21:32 +0100

    question 1 done

--- questions-1-5.js
+++ questions-1-5.js
@@ -1,1 +1,40 @@
-
+<!-- questionn 1 begins here -->
+<script type="text/javascript">
+let today = new Date();
+  let day = today.getDay();
+  let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log("Today is : " + daylist[day] + ".");
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  let period = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && period===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  period=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  period=' PM';
+  } 
+  } 
+  if (hour===0 && period===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  period=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  period=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + period + " : " + minute + " : " + second);
+	</script>
+<!--question 1 ends Here -->
