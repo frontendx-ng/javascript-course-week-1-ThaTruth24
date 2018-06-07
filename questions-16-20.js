1a16cccdfae2e1564b06657b15284e5742edf7c7
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 13:05:26 +0100

    Question 17 Done

--- questions-16-20.js
+++ questions-16-20.js
@@ -34,3 +34,41 @@ function sum() {
 </body>
 </html>
 <!-- Question 16 Ends Here -->
+
+	
+<!--Question 17 Starts Here -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
+<form>
+	Enter A Number: <input type="text" id="number"/><br>
+	
+	<input type="button" onClick="difference()" Value="Get Difference" />
+</form>
+
+<script>
+
+function difference() {
+ 	let num1 = document.getElementById("number").value;
+
+    if (num1 > 19){
+        answer = (+num1 - 19) * 3;
+    }
+    else{
+        answer = (19 - +num1);
+    }
+
+    console.log(answer);
+ }
+
+
+</script>
+
+</body>
+</html>
+
+<!-- Question 17 Ends Here-->
