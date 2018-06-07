72d73f9cbc22d2d231f0cfc3fcf4800a2f71fe74
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 13:36:14 +0100

    Question 21 Done

--- questions-21-25.js
+++ questions-21-25.js
@@ -1,1 +1,37 @@
+<!-- Question 21 Starts Here -->
+ 
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
+<form>
+	Enter A String: <input type="text" id="string"/><br>
 
+	<input type="button" onClick="check()" Value="check" />
+</form>
+
+<script>
+
+function check() {
+ 	let str = document.getElementById("string").value;
+
+    if (str.substring(0 , 2) === 'Py'){
+        answer = str;
+    }
+    else{
+        answer = "Py" + str;
+    }
+
+    console.log(answer);
+ }
+
+
+</script>
+
+</body>
+</html>
+
+<!-- QUestion 21 Ends Here -->
+  
