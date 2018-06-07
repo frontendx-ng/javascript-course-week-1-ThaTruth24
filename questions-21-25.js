de9f48c1c8e8a277a935e8808a2fd47e9fd5615d
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 14:36:23 +0100

    Question 22 Done

--- questions-21-25.js
+++ questions-21-25.js
@@ -35,3 +35,32 @@ function check() {
 
 <!-- QUestion 21 Ends Here -->
   
+<!--QUestion 22 Starts Here -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
+<form>
+	Enter A String: <input type="text" id="str"/><br>
+	Enter Position: <input type="text" id="pos"/><br>
+
+	<input type="button" onClick="removeChar()" Value="check" />
+</form>
+
+<script>
+
+function removeChar() {
+	let str = document.getElementById("str").value
+	let pos = document.getElementById("pos").value
+console.log( str.replace(str.substring(pos - 1, pos), ''));
+}
+
+
+</script>
+
+</body>
+</html>
+
+<!-- Question 22 Ends Here -->
