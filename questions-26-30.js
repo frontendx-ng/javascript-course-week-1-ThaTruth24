325176b5d152360a1d40c4d32e6f48bab278ac7b
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 15:30:35 +0100

    Question 26 Done

--- questions-26-30.js
+++ questions-26-30.js
@@ -1,1 +1,33 @@
+<!--Question 26 Starts Here -->
+ 
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
+<form>
+	Enter A String: <input type="text" id="str"/><br>
+
+	<input type="button" onClick="swap()" Value="Swap" />
+</form>
+
+<script>
+
+function swap() {
+  	let str = document.getElementById("str").value
+  	if (str.length >=3) {
+  	let last3 = str.substring(str.length-3);
+  console.log( last3 + str + last3 );
+} else
+	console.log("String Must Be 3 Characters And Above");
+}
+
+</script>
+
+</body>
+</html>
+
+<!--Question 26 Ends Here -->
+  
 
