81fedae7f6750772c90c436aa4a7e6f0d5f21575
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 13:10:03 +0100

    Question 18 Done

--- questions-16-20.js
+++ questions-16-20.js
@@ -72,3 +72,40 @@ function difference() {
 </html>
 
 <!-- Question 17 Ends Here-->
+	
+<!--Question 18 STarts Here-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
+<form>
+	Enter 1st Number: <input type="text" id="number1"/><br>
+	Enter 2nd Number: <input type="text" id="number2"/><br>
+
+	<input type="button" onClick="check()" Value="Add Numbers" />
+</form>
+
+<script>
+
+function check() {
+ 	let num1 = document.getElementById("number1").value;
+ 	let num2 = document.getElementById("number2").value;
+
+    if ((num1 || num2) == 50 || (+num1 + +num2) == 50){
+        answer = true;
+    }
+    else{
+        answer = false;
+    }
+
+    console.log(answer);
+ }
+
+
+</script>
+
+</body>
+</html>
+<!-- Question 18 Ends Here -->
