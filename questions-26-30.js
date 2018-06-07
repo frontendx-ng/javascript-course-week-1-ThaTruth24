6c7d36aa67cf696f85ddb44086ca6199b6a1d7f4
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 15:54:21 +0100

    Question 28 Done

--- questions-26-30.js
+++ questions-26-30.js
@@ -63,3 +63,38 @@ function check() {
 </html>
 
 <!-- QUestion 27 Ends Here -->
+
+	
+<!--Question 28 Starts Here -->
+	
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
+<form>
+	Enter Number1: <input type="text" id="num1"/><br>
+	Enter Number2: <input type="text" id="num2"/><br>
+
+	<input type="button" onClick="check()" Value="Check" />
+</form>
+
+<script>
+
+function check() {
+  	let num1 = document.getElementById("num1").value;
+  	let num2 = document.getElementById("num2").value;
+
+  	if ((num1 >= 50 && num1 <= 99  ) || (num2 >= 50 && num2 <= 99)) {
+  console.log(true);
+} else
+	console.log(false);
+}
+
+</script>
+
+</body>
+</html>
+
+<!--Question 28 Ends Here -->
