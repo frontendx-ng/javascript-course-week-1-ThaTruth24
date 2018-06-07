9b2816f6ebec44739afd84f6995c1df1e1b6690b
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 11:16:27 +0100

    Question 11 Done

--- questions-11-15.js
+++ questions-11-15.js
@@ -1,1 +1,38 @@
+<!--question 11 Starts Here -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
 
+<form>
+
+Enter A Number : <input type="text" id="value" /><br>
+
+<input type="button" onClick="cToFahr()" Value="Convert To Fahrenheit" />
+<input type="button" onClick="fToCel()" Value="Convert To Celcius" />
+</form>
+
+
+<script>
+function cToFahr() 
+{
+  var cTemp = document.getElementById("value").value;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
+
+function fToCel() 
+{
+  var fTemp = document.getElementById("value").value;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+</script>
+
+</body>
+</html>
+<!--Question 11 ends here -->
