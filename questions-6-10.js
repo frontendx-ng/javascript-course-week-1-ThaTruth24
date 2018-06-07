9e7469a29121f2318710a5d413c3b31d1d48c083
ThankGod Egbo <34455592+ThaTruth24@users.noreply.github.com>
2018-06-07 09:42:00 +0100

    Question 6 done

--- questions-6-10.js
+++ questions-6-10.js
@@ -1,1 +1,25 @@
+<!--- question 6 starts here -->
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Frontend X</title>
+</head>
+<body>
 
+<script>
+year = window.prompt("Input a Year : ");
+x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+if (x == true) {
+console.log("This Year Is A Leap Year");
+}
+else{
+	console.log("This Year Is Not A Leap Year");
+}
+</script>
+
+</body>
+</html>
+
+<!-- question 6 ends here -->
+  
+  
